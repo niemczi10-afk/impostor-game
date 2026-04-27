6e8992dc47ca809c239fb79cbe2f127e68e94c94
niemczi10-afk <niemczi10@gmail.com>
2026-04-27 06:15:56 +0200

    Create words.js

---
+++ words.js
@@ -0,0 +1,12 @@
+const words = [
+  { word: "pies", hint: "zwierzę" },
+  { word: "samochód", hint: "transport" },
+  { word: "pizza", hint: "jedzenie" },
+  { word: "telefon", hint: "technologia" },
+  { word: "plaża", hint: "wakacje" },
+  { word: "szkoła", hint: "miejsce" },
+  { word: "kawa", hint: "napój" },
+  { word: "las", hint: "natura" },
+];
+
+module.exports = words;
